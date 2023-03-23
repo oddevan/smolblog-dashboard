15bbde990293684d308eeb05ab47f70adeb7969f
Evan Hildreth <me@eph.me>
2023-03-23 18:51:53 -0400

    Statically render plz

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import context from "$lib/stores/context";
 import type { LayoutLoad } from "./$types";
 
+export const prerender = true;
+
 export const load = (() => {
 	context.init('//smolblog.test/wp-json/smolblog/v2');
 	context.setAuthHeader('Basic 12334567890asdfghjkl');
