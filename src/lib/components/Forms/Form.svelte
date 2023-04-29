1ebda857205ebc6024433fac95f0bf1f155d5fb4
Evan Hildreth <me@eph.me>
2023-04-29 13:57:57 -0400

    Refactor form component

--- src/lib/components/Forms/Form.svelte
+++ src/lib/components/Forms/Form.svelte
@@ -1,22 +1,24 @@
 <script lang="ts" context="module">
-	import { onMount, type ComponentType, getContext } from "svelte";
-
 	export interface FormField {
 		name: string,
 		label: string,
 		type: 'text'|'display',
 		description?: string,
-		component?: ComponentType,
 		required?: boolean,
+	};
+
+	export interface FormState {
+		saving: boolean,
+		success: boolean,
+		saveError?: Error,
 	};
 </script>
 
 <script lang="ts">
   import { form as makeForm, field as makeField, type Validator } from 'svelte-forms';
   import { required as requiredValidator } from 'svelte-forms/validators';
-	import TextField from './TextField.svelte';
-	import DisplayField from './DisplayField.svelte';
-	import ErrorBox from '../ErrorBox.svelte';
+	import { writable } from "svelte/store";
+	import CardForm from './CardForm.svelte';
 
 	export let definition: FormField[];
 	export let initialData: any = {};
@@ -34,58 +36,29 @@
 	const formController = makeForm(...fields)
 
 	export let setter: ((arg0: any) => Promise<any>)|undefined;
-	let enableSave = true;
-	let saving = false;
-	let success = false;
-	let setError: Error|undefined;
+
+	const state = writable<FormState>({
+		saving: false,
+		success: false,
+	});
 
 	const handleSubmit = async () => {
-		success = false;
-		setError = undefined;
+		if (!setter) { return; }
 
-		formController.validate();
-		if ($formController.valid && setter) {
-			saving = true;
-			try {
+		state.set({ saving: true, success: false });
+
+		try {
+			formController.validate();
+			if ($formController.valid) {
 				await setter($formController.summary);
-				saving = false;
-				success = true;
-			} catch (e) {
-				setError = e as Error;
+				state.set({ saving: false, success: true });
 			}
+		} catch (e) {
+			state.set({ saving: false, success: false, saveError: e as Error});
 		}
 	}
-
-	$: enableSave = !saving && $formController.dirty;
-
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
-	<div class="card">
-		<div class="card-body">
-		{#each definition as field (field.name)}
-			{#if field.type == 'text'}
-				<TextField definition={field} controller={formController.getField(field.name)} />
-			{:else if field.type == 'display'}
-				<DisplayField definition={field} controller={formController.getField(field.name)} />
-			{/if}
-		{/each}
-		<ErrorBox error={setError}/>
-		</div>
-		<div class="card-footer d-flex">
-			{#if success}
-				<span class="text-success">Saved successfully!</span>
-			{/if}
-			{#if setter}
-			<button class="btn btn-primary ms-auto" disabled={!enableSave}>
-				{#if saving}
-				<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-				Saving...
-				{:else}
-				Save
-				{/if}
-			</button>
-			{/if}
-		</div>
-	</div>
+	<CardForm {state} {definition} {formController}/>
 </form>
---
+++ src/routes/site/[handle]/settings/permissions/UserPerm.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { UserAndPermissions } from "$lib/smolblog/types";
+
+	export let line: UserAndPermissions;
+	const { user, isAdmin, isAuthor } = line;
+</script>
+
+<div class="row">
+	<div class="col-sm-6">
+		{user.displayName} ({user.handle})
+	</div>
+	<div class="col-xs-4 col-sm-2">
+		<div class="form-check form-switch">
+			<label class="form-check-label" for="canPushSwitch">
+				Push
+			</label>
+			<input class="form-check-input" type="checkbox" role="switch" id="canPushSwitch" bind:checked={push}>
+		</div>
+	</div>
+	<div class="col-xs-4 col-sm-2">
+		<div class="form-check form-switch">
+			<label class="form-check-label" for="canPullSwitch">
+				Pull
+			</label>
+			<input class="form-check-input" type="checkbox" role="switch" id="canPullSwitch" bind:checked={pull}>
+		</div>
+	</div>
+	<div class="col-xs-4 col-sm-2">
+		<button class="btn btn-sm btn-outline-primary" disabled={!enabled} on:click={save}>Save</button>
+	</div>
+</div>
