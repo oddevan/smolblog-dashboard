ec539dbdfc00fcbe63864bcd5a2b57ae1edc4140
Evan Hildreth <me@eph.me>
2023-04-30 10:26:34 -0400

    Use netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+// import staticAdapter from '@sveltejs/adapter-static';
+import netlifyAdapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({ fallback: '200.html' })
+		// adapter: staticAdapter({ fallback: 'index.html' })
+		adapter: netlifyAdapter({ fallback: 'index.html' })
 	}
 };
 
