eee6cfdd1e7d40e2e7d3889aacbd86f1f1848f6c
Evan Hildreth <me@eph.me>
2023-06-05 09:41:49 -0400

    Better site links

--- src/lib/components/SiteDisplay.svelte
+++ src/lib/components/SiteDisplay.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Avatar } from 'flowbite-svelte';
+	import { Avatar, Card } from 'flowbite-svelte';
 	import type { Site } from "$lib/smolblog/types";
 
 	export let site: Site;
@@ -7,14 +7,16 @@
 	const siteServer = (new URL(site.baseUrl)).hostname;
 </script>
 
-<div class="flex items-center p-1 space-x-4 bg-white dark:bg-gray-900">
+<Card href="/site/{site.handle}" horizontal padding="none">
+<div class="flex items-center p-3 overflow-hidden space-x-4">
 	<Avatar rounded>
 		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 			<path stroke-linecap="round" stroke-linejoin="round" d="M3 8.25V18a2.25 2.25 0 002.25 2.25h13.5A2.25 2.25 0 0021 18V8.25m-18 0V6a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 6v2.25m-18 0h18M5.25 6h.008v.008H5.25V6zM7.5 6h.008v.008H7.5V6zm2.25 0h.008v.008H9.75V6z" />
 		</svg>
 	</Avatar>		
 	<div class="space-y-1 font-medium dark:text-white">
-			<div>{site.displayName}</div>
+			<div class="truncate">{site.displayName}</div>
 			<div class="text-sm secondary-text">{siteServer}</div>
 	</div>
 </div>
+</Card>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -69,14 +69,12 @@
 			</svg>
 		</Avatar>
 		{/if}
-		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 -mr-1">
+		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 -mr-2">
 			<path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
 		</svg>
 	</Button>
 	<MegaMenu full items={menuSites ?? []} let:item>
-		<a href={item.href} class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700 h-full">
-			<SiteDisplay site={item.site}/>
-		</a>
+		<SiteDisplay site={item.site} />
 		<div slot="extra" class="">
 			<Sidebar>
 				<SidebarWrapper>
