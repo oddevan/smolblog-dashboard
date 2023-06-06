685996fe8b1d11a32096f4522fd1e2aa3bfe42f2
Evan Hildreth <me@eph.me>
2023-06-05 22:23:36 -0400

    Add speed dial

--- src/routes/site/[handle]/+layout.svelte
+++ src/routes/site/[handle]/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { BottomNav, BottomNavItem, Button, Modal, Sidebar, SidebarGroup, SidebarItem, SidebarWrapper, Tooltip } from "flowbite-svelte";
+	import { BottomNav, BottomNavItem, Button, Modal, Sidebar, SidebarGroup, SidebarItem, SidebarWrapper, SpeedDial, SpeedDialButton, Tooltip } from "flowbite-svelte";
 	import type { LayoutData } from "./$types";
 	import Subnav from "$lib/components/Subnav.svelte";
 	import PageTitle from "$lib/components/PageTitle.svelte";
@@ -52,6 +52,20 @@
 				</SidebarGroup>
 			</SidebarWrapper>
 		</Sidebar>
+
+		<SpeedDial defaultClass="mt-3" tooltip="none" placement="bottom" textOutside>
+			{@const textOutsideClass = 'block absolute left-14 top-1/2 mb-px text-sm font-medium -translate-y-1/2'}
+			<SpeedDialButton name="Status" {textOutsideClass}>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+				</svg>
+			</SpeedDialButton>
+			<SpeedDialButton name="Reblog" {textOutsideClass}>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
+				</svg>
+			</SpeedDialButton>
+		</SpeedDial>
 	</div>
 	<div class="grow">
 		<Subnav items={$page.data.subnav}/>
@@ -102,9 +116,7 @@
 	<h3>Create new content</h3>
   <div class="grid grid-cols-2 gap-4 place-content-center">
     <Button href="content/new/status">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-				<path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-			</svg><br/>
+			<br/>
 			Status
 		</Button>
     <Button href="content/new/reblog">
