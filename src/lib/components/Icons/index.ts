b76172e7231b28ef3c8ba145425f555329730c78
Evan Hildreth <me@eph.me>
2023-09-21 19:04:09 -0400

    More Icons

---
+++ src/lib/components/Icons/index.ts
@@ -0,0 +1,13 @@
+export { default as Alert } from './Alert.svelte';
+export { default as ArrowOut } from './ArrowOut.svelte';
+export { default as Content } from './Content.svelte';
+export { default as Home } from './Home.svelte';
+export { default as Inbox } from './Inbox.svelte';
+export { default as Markdown } from './Markdown.svelte';
+export { default as Reader } from './Reader.svelte';
+export { default as Reblog } from './Reblog.svelte';
+export { default as Settings } from './Settings.svelte';
+export { default as Site } from './Site.svelte';
+export { default as Snek } from './Snek.svelte';
+export { default as Status } from './Status.svelte';
+export { default as User } from './User.svelte';
