e570a6ea5b700ea284a7ff713cfbe6679a8c6d5b
Evan Hildreth <me@eph.me>
2023-09-22 22:21:37 -0400

    Attempting some work

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'smolblog-dashboard',
+  appId: 'com.smolblog.dashboard',
+  appName: 'Smolblog',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
