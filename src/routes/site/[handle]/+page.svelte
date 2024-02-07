bc23b7e2b9cf0ed10e4f79ec118f191e5aed1a0c
Evan Hildreth <me@eph.me>
2024-02-06 19:01:22 -0500

    Actually edit drafts on the blog dashboard

--- src/routes/site/[handle]/+page.svelte
+++ src/routes/site/[handle]/+page.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
 	import ContentForm from "$lib/components/ContentForm.svelte";
-import ContentIcon from "$lib/components/ContentIcon.svelte";
+	import ContentIcon from "$lib/components/ContentIcon.svelte";
 	import smolblog from "$lib/smolblog";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
 	$: siteApi = smolblog(data.context).site(data.site?.id);
+	let contentId: string|null = null;
 </script>
 
 <div class="grid grid-cols-1 md:!grid-cols-2">
 	{#key data}
 	<div class="px-4">
 		{#if siteApi}
-		<h3 class="h3 mb-4">Make something new</h3>
-		<ContentForm {siteApi} closeFunction={() => goto(`/site/${data.site?.handle}/content`)} />
+		{#if !contentId}<h3 class="h3 mb-4">Make something new</h3>{/if}
+		<ContentForm {siteApi} {contentId} closeFunction={() => goto(`/site/${data.site?.handle}/content`)} />
 		{/if}
 	</div>
 	<div class="list-nav card p-4">
@@ -22,10 +23,10 @@ import ContentIcon from "$lib/components/ContentIcon.svelte";
 		<ul>
 			{#each data.drafts ?? [] as content (content.id)}
 			<li>
-				<a href={`/site/${data.site?.handle ?? '0'}/edit/${content.id}`} class="flex">
+				<button on:click={() => contentId = content.id} class="flex">
 					<span><ContentIcon type={content.contentType.type} size="medium" class="me-2"/></span>
 					<span class="flex-auto">{content.title}</span>
-				</a>
+				</button>
 			</li>
 			{/each}
 		</ul>
