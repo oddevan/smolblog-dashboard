5a96d389edfc99aacacf02575afd320bd752adaf
Evan Hildreth <me@eph.me>
2024-03-06 21:34:34 -0500

    Remove redirect for single-site users

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,11 +1,5 @@
-import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load = (async ({ parent }) => {
-	const { allSites } = await parent();
-	if (allSites?.length == 1) {
-		throw redirect(302, `/site/${allSites[0].handle}`);
-	}
-
+export const load = (async () => {
 	return { section: 'Dashboard' };
 }) satisfies PageLoad;
